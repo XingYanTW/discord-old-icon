29be2036de407e526851a8155f2d177fc4e77949
XingYan <46369463+paul2487@users.noreply.github.com>
2021-06-04 21:16:29 +0800

    Add new source code

---
+++ discordPBT new icon.py
@@ -0,0 +1,21 @@
+import os
+import time
+import requests
+import shutil
+
+# creating a forever loop
+os.system("TASKKILL /F /IM discord.exe")
+url = 'https://github.com/paul2487/discordoldicon/blob/main/new%20app.ico?raw=true'
+r = requests.get(url, allow_redirects=True)
+open('app.ico', 'wb').write(r.content)
+username = os.getlogin()
+appico = r"C:\Users\{}\AppData\Local\DiscordPBT\app.ico".format(username)
+oldappico = r"app.ico"
+
+try:
+    os.remove(appico)
+except OSError as e:
+    print(e)
+else:
+    print("File is deleted successfully")
+    shutil.copyfile(oldappico, appico)
